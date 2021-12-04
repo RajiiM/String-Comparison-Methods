28a7e4af24fdb79f928377b6addf0c255149c926
RajiiM <mrajalakshmiofficial@gmail.com>
2021-12-05 00:30:21 +0530

    FirstCommit

---
+++ endsWith.java
@@ -0,0 +1,14 @@
+public class Practice {
+
+	public static void main(String[] args) {
+      String s1 = "Java";
+      String s2 = new String("JAVALEARNING");
+
+      System.out.println(s1.endsWith("A"));  
+      System.out.println(s2.endsWith("ING"));  
+	}
+}
+
+//OUTPUT
+false
+true
