3f6195e96af25edce617987835b9d80db04f51d1
RajiiM <mrajalakshmiofficial@gmail.com>
2021-12-05 00:29:42 +0530

    FirstCommit

---
+++ startsWith.java
@@ -0,0 +1,14 @@
+public class Practice {
+
+	public static void main(String[] args) {
+      String s1 = "Java";
+      String s2 = new String("JAVALEARNING");
+
+      System.out.println(s1.startsWith("J"));  
+      System.out.println(s2.startsWith("L",4));  
+	}
+}
+
+//OUTPUT
+true
+true
