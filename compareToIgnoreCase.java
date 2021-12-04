11945ba8589327a3d28aeb8ac146b915058c3a73
RajiiM <mrajalakshmiofficial@gmail.com>
2021-12-04 23:05:37 +0530

    FirstCommit

---
+++ compareToIgnoreCase.java
@@ -0,0 +1,13 @@
+public class Practice {
+
+	public static void main(String[] args) {
+      String s1 = "Java";
+      String s2 = new String("JAVA");
+
+      System.out.println(s1.compareToIgnoreCase(s2));    
+	}
+}
+
+
+//OUTPUT
+0
