489abc3bfe5b13d4e128fe032a08e34b36a525fd
RajiiM <mrajalakshmiofficial@gmail.com>
2021-12-04 23:04:41 +0530

    FirstCommit

---
+++ equalsIgnoreCase.java
@@ -0,0 +1,12 @@
+public class Practice {
+
+	public static void main(String[] args) {
+      String s1 = "RaJaLAKshmi";
+      String s2 = new String("rajalakshmi");
+      
+      System.out.println(s1.equalsIgnoreCase(s2));     
+	}
+}
+
+//OUTPUT
+true
